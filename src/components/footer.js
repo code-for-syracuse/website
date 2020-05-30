8dddb593688091f36988a42dbf93c8deaa5e59a9
Jason Scharf <jasonscharf3@gmail.com>
2020-05-30 19:45:03 -0400

    added footer text

---
+++ src/components/footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './intro.css';
+
+const Footer = () => (
+  <div className="footer-wrapper">
+    <div className="footer-left-column">
+      <h1>
+        <span className="footer-big">Code For Syracuse</span>
+        </h1>
+        <p className="footer-byline">
+        About
+      </p>
+      <p className="footer-byline">
+        Volunteer
+      </p>
+      <p className= "footer-byline">
+        Civic Data Sources
+      </p>
+    </div>
+ 
+<div className="footer-right-column">
+<h1>
+        <span className="footer-big">Contact Us</span>
+        </h1>
+        <p className="footer-byline">
+        Email: <a href="mailto: hello@codeforsyracuse.org"></a> 
+      </p>
+      <p className="footer-byline">
+      Slack: Join the <a href="https://slackacuse.herokuapp.com/">Syracuse.io</a> Slack group
+      </p>
+      <p className= "footer-byline">
+      Twitter: <a href="https://twitter.com/codeforsyracuse">@CodeForSyracuse</a> 
+      </p>
+      <p className= "footer-byline">
+      Meetup: <a href="https://www.meetup.com/Code-for-Syracuse/">Code For Syracuse</a>
+      </p>
+</div>
+</div>
+)
+
+export default Footer
